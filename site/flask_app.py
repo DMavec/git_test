7a242507b6642a5c06726651507c6fd666168c9b
dariomavec <dario.mavec@gmail.com>
2017-08-02 02:53:27 +0000

    Adding site elements

--- site/flask_app.py
+++ site/flask_app.py
@@ -2,10 +2,11 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__, static_url_path = '')
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
-
+    return render_template('index.html',
+                            title='Home')
