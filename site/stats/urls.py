0e6e11f1726f7c8662aab32600b68c41fe592996
dariomavec <dario.mavec@gmail.com>
2018-04-03 14:02:27 +1000

    Removing old Django site - moving to new framework (Django REST API backend with React frontend)

--- site/stats/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'stats'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:player_name>/', views.player_stats, name='player_stats')
-]
