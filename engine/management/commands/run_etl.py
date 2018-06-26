0673e1935b1fc580c1760e6808d75fb0a165bd6c
Dario Mavec <dario.mavec@gmail.com>
2018-06-26 21:52:28 +1000

    Added TODO into run_etl

--- engine/management/commands/run_etl.py
+++ engine/management/commands/run_etl.py
@@ -8,6 +8,5 @@ class Command(BaseCommand):
       Run on all available data from api. By default only checks the last 100 games.'''
 
     def handle(self, *args, **options):
-
-
+        ## TODO: Add optional argument to run the full load
         run_etl()
